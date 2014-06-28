527e4bddd200d0a3b1e3eae005dbcf74633efce8
rajajhansi <rajajhansi@yahoo.com>
2014-06-28 11:07:42 -0700

    Added a shade of blue to all icons on the left nav bar and removed justify from CSS style tamil. Changed the selection of menu items to use data-ng-class. Added .less files into Github.

---
+++ app/layout/SideNavBarController.js
@@ -0,0 +1,29 @@
+﻿(function () {
+    'use strict';
+
+    var controllerId = 'SideNavBarController';
+
+    // TODO: replace app with your module name
+    angular.module('mainApp').controller(controllerId,
+        ['$scope', '$routeParams', '$location', SideNavBarController]);
+
+    function SideNavBarController($scope, $routeParams, $location) {
+        var vm = this;
+
+        vm.activate = activate;
+        vm.title = 'SideNavBarController';
+        vm.getClass = function (path) {            
+            var currentPath = $location.path().substr(0, path.length);
+            if (currentPath == path) {
+                if ($location.path().substr(0).length > 1 && path.length == 1)
+                    return "";
+                else {
+                    return "stick active";
+                }
+            } else {
+                return "";
+            }
+        }
+        function activate() { }
+    }
+})();
